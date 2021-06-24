49830cfff7e2f99568fcb93a4223cc691b8a4250
Sanyam <sanyambhaskar5@gmail.com>
2021-06-24 23:32:00 +0530

    Added Plugin

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { gsap, CSSPlugin } from "gsap/all";
+gsap.registerPlugin(CSSPlugin);
 
 ReactDOM.render(
 
